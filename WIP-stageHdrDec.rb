f885b899d16abf9d687a7fd03a13af2ce5b48ca9
SecaProject <esazona@hotmail.com>
2016-07-09 00:38:51 +0200

    Update WIP-stageHdrDec.rb

--- WIP-stageHdrDec.rb
+++ WIP-stageHdrDec.rb
@@ -12,6 +12,5 @@ for loop in 1..512-1
  bytes_to_decode = IO.binread(file, 4, loop * 4).unpack("V*").join.to_i
  save.write [(bytes_to_decode ^ to_xor).to_i.to_s(16).split(//).last(8).join.rjust(8, "0").scan(/(..)(..)(..)(..)/).map(&:reverse).join].pack("H*")
  to_xor = seed + (xor_key ^ 0xf0f0)
- puts
 end
 save.close
